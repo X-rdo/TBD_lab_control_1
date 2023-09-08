2151e3cf3875426fe182bfddf7ab50591c733cd0
Nachowo <ignacio.arias.t@usach.cl>
2023-09-08 20:58:37 -0300

    dbCreate terminado

--- dbCreate.sql
+++ dbCreate.sql
@@ -27,15 +27,6 @@ CREATE TABLE IF NOT EXISTS Cliente(
     FOREIGN KEY (comuna_fk) REFERENCES Comuna(id_comuna)
 );
 
-CREATE TABLE IF NOT EXISTS Cliente(
-    id_cliente_vehiculo serial NOT null,
-    cliente_fk bigint,
-    vehiculo_fk bigint,
-    PRIMARY KEY (id_cliente_vehiculo),
-    FOREIGN KEY (cliente_fk) REFERENCES Lugar_cliveh(id_lugar_cliveh),
-    FOREIGN KEY (vehiculo_fk) REFERENCES Vehiculo(id_vehiculo)
-);
-
 CREATE TABLE IF NOT EXISTS Edificio_estacionamiento
 (
     id_edificio_estacionamiento SERIAL PRIMARY KEY,
@@ -50,4 +41,59 @@ CREATE TABLE IF NOT EXISTS Lugar(
     edificios_estacion_fk bigint,
     PRIMARY KEY (id_lugar),
     FOREIGN KEY (edificios_estacion_fk) REFERENCES Edificio_estacionamiento
+);
+
+CREATE TABLE IF NOT EXISTS Cliente_vehiculo(
+    id_cliente_vehiculo serial NOT null,
+    cliente_fk bigint,
+    vehiculo_fk bigint,
+    PRIMARY KEY (id_cliente_vehiculo),
+    FOREIGN KEY (cliente_fk) REFERENCES Cliente(id_cliente),
+    FOREIGN KEY (vehiculo_fk) REFERENCES Vehiculo(id_vehiculo)
+);
+
+CREATE TABLE if NOT EXISTS Lugar_cliveh(
+    id_lugar_cliveh serial NOT null,
+    lugar_fk bigint,
+    cliente_vehiculo_fk bigint,
+    PRIMARY KEY (id_lugar_cliveh),
+    FOREIGN KEY (lugar_fk) REFERENCES Lugar(id_lugar),
+    FOREIGN KEY (cliente_vehiculo_fk) REFERENCES Cliente_vehiculo(id_cliente_vehiculo)
+);
+
+CREATE TABLE Pago
+(
+    id_pago SERIAL PRIMARY KEY,
+    monto MONEY
+);
+
+CREATE TABLE Sueldo
+(
+    id_sueldo SERIAL PRIMARY KEY,
+    dinero MONEY
+);
+
+CREATE TABLE IF NOT EXISTS Contrato
+(
+    id_contrato SERIAL PRIMARY KEY,
+    fecha TIMESTAMP,
+    cliente_vehiculo_fk BIGINT,
+    edificio_estacionamiento_fk BIGINT,
+    pago_fk SERIAL,
+    FOREIGN KEY (cliente_vehiculo_fk) REFERENCES Cliente_vehiculo(id_cliente_vehiculo) ,
+    FOREIGN KEY (edificio_estacionamiento_fk) REFERENCES Edificio_estacionamiento(id_edificio_estacionamiento),
+    FOREIGN KEY (pago_fk) REFERENCES Pago(id_pago)
+);
+
+CREATE TABLE Empleado
+(
+    id_empleado SERIAL PRIMARY KEY,
+    nombre TEXT,
+    cargo TEXT,
+    sueldo_fk BIGINT,
+    comuna_fk BIGINT,
+    edificio_estacionamiento_fk BIGINT,
+    FOREIGN KEY (sueldo_fk) REFERENCES Sueldo(id_sueldo),
+    FOREIGN KEY (comuna_fk) REFERENCES Comuna(id_comuna),
+    FOREIGN KEY (edificio_estacionamiento_fk) REFERENCES Edificio_estacionamiento(id_edificio_estacionamiento)
 );
