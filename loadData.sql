6c981d016d132aef313a7a32c7d06a94cc4c5607
Alejandro Aldea <alejandro.aldea@usach.cl>
2023-09-09 14:05:45 -0300

    [Alejandro Aldea] - Actualización Script para cargar datos

--- loadData.sql
+++ loadData.sql
@@ -1,39 +1,45 @@
+ALTER SEQUENCE Modelo_id_modelo_seq RESTART WITH 1;
+ALTER SEQUENCE vehiculo_id_vehiculo_seq RESTART WITH 1;
+ALTER SEQUENCE comuna_id_comuna_seq RESTART WITH 1;
+ALTER SEQUENCE cliente_id_cliente_seq RESTART WITH 1;
+ALTER SEQUENCE Edificio_estacionamiento_id_edificio_estacionamiento_seq RESTART WITH 1;
 
-INSERT INTO public.modelo(id_modelo, marca, ModeloVe) VALUES(1, 'BMW','128i'),
-(2, 'Mercedes','A200'), 
-(3, 'McLaren', '570 Gt'),
-(4, 'Nissan', 'Skyline R4'), 
-(5, 'Honda', 'CIVIC');
-
-INSERT INTO public.vehiculo(id_vehiculo, patente, modelo_fk) VALUES(1, 'WXYZ34',1),
-(2, 'LMNO78',2), 
-(3, 'EFGH56', 3),
-(4, 'QRST90', 4), 
-(5, 'ABHD12', 5);
+INSERT INTO public.modelo(marca, ModeloVe) VALUES('BMW','128i'),
+('Mercedes','A200'), 
+('McLaren', '570 Gt'),
+('Nissan', 'Skyline R4'), 
+('Honda', 'CIVIC');
 
 
-INSERT INTO public.comuna(id_comuna, nombre_comuna) VALUES(1, 'LO BARNECHEA'),
-(2, 'VITACURA'), 
-(3, 'MAIPÚ'),
-(4, 'PROVIDENCIA'), 
-(5, 'LAS CONDES'),
-(6, 'ESTACIÓN CENTRAL'),
-(7, 'MAIPÚ'),
-(8, 'SAN BERNARDO'),
-(9, 'LA FLORIDA'),
-(10, 'INDEPENDENCIA');
+INSERT INTO public.vehiculo( patente, modelo_fk) VALUES('WXYZ34',1),
+('LMNO78',2), 
+('EFGH56', 3),
+('QRST90', 4), 
+('ABHD12', 5);
 
-INSERT INTO public.cliente(id_cliente, nombre, comuna_fk) VALUES(1, 'Alejandro', 5),
-(2, 'Yerko', 1), 
-(3, 'Ignacio', 2),
-(4, 'Ricardo', 3), 
-(5, 'Pablo', 4);
 
-INSERT INTO public.Edificio_estacionamiento(id_edificio_estacionamiento, cantidad_estacionamientos, comuna_fk) VALUES(1, 5, 5),
-(2, 10, 1), 
-(3, 1, 2),
-(4, 2, 3), 
-(5, 7, 4);
+INSERT INTO public.comuna(nombre_comuna) VALUES('LO BARNECHEA'),
+('VITACURA'), 
+('MAIPÚ'),
+('PROVIDENCIA'), 
+('LAS CONDES'),
+('ESTACIÓN CENTRAL'),
+('MAIPÚ'),
+('SAN BERNARDO'),
+('LA FLORIDA'),
+('INDEPENDENCIA');
+
+INSERT INTO public.cliente(nombre, comuna_fk) VALUES( 'Alejandro', 5),
+('Yerko', 1), 
+('Ignacio', 2),
+('Ricardo', 3), 
+('Pablo', 4);
+
+INSERT INTO public.Edificio_estacionamiento(cantidad_estacionamientos, comuna_fk) VALUES(5, 5),
+(10, 1), 
+(1, 2),
+(2, 3), 
+(7, 4);
 
 
 
