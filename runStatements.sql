2fa791cb894f1e31d9c7b950bca1cb9970c0c3cb
Gabriel Ojeda Romero <gabriel.ojeda@usach.cl>
2023-09-16 15:32:46 -0300

    [Gabriel Ojeda] - Corrección sentencia 1

--- runStatements.sql
+++ runStatements.sql
@@ -1,12 +1,12 @@
 -- 1. Lista de clientes que gasta más por edificio.
 -- COLUMNAS: id_cliente, nombre_cliente, id_edificio, monto_total_gastado.
-SELECT E.id_cliente AS id_cliente, C.nombre AS nombre_cliente, EE.id_edificio_estacionamiento AS id_edificio, SUM(P.monto) AS monto_total_gastado
+SELECT C.id_cliente AS id_cliente, C.nombre AS nombre_cliente, EE.id_edificio_estacionamiento AS id_edificio, SUM(P.monto) AS monto_total_gastado
 FROM Cliente C
     JOIN Cliente_vehiculo CV ON C.id_cliente = CV.cliente_fk                                             --Se filtra la tabla cliente vehiculo, con la id del cliente.
     JOIN Contrato Co ON CV.id_cliente_vehiculo = Co.cliente_vehiculo_fk                                  --Se extrae todos los contratos de ese cliente.
     JOIN Edificio_estacionamiento EE ON Co.edificio_estacionamiento_fk = EE.id_edificio_estacionamiento  --Se busca los edificios en donde ha generado gasto con los contratos encontrados.
     JOIN Pago P ON Co.pago_fk = P.id_pago                                                                --Se extrae el pago.
-	GROUP BY E.id_cliente, C.nombre, EE.id_edificio_estacionamiento                                              --Se agrupa por cliente y edificio.
+	GROUP BY C.id_cliente, C.nombre, EE.id_edificio_estacionamiento                                              --Se agrupa por cliente y edificio.
 ORDER BY EE.id_edificio_estacionamiento, monto_total_gastado DESC;                                     --Se agrupa en orden desendiente por el monto total gastado.
 
 
